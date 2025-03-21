0cc1d0c71b72308e246625581cfec959854d653e
jennifer-fintlabs <jennifer@fintlabs.no>
2025-03-21 12:28:14 +0100

    testing for styling 🐰

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import { NovariIKS } from "./logo/NovariIKS";  // ✅ Ensure relative path
 export { default as NovariHeader } from "./components/NovariHeader";
 export { default as NovariFooter } from "./components/NovariFooter";
+export { default as NovariInternalHeader} from "./components/NovariInternalHeader";
 
 
 export { NovariIKS };
