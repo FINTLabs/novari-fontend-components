a5582f5e0565746943b7a56a4168d81efae28253
sindre <sindre.giske@novari.no>
2025-08-04 12:57:20 +0200

    [CircularProgressBar] Story description

--- src/components/NovariCircularProgressBar.stories.tsx
+++ src/components/NovariCircularProgressBar.stories.tsx
@@ -7,6 +7,13 @@ const meta = {
     tags: ["autodocs", "experimental"],
     parameters: {
         layout: 'centered',
+        docs: {
+            description: {
+                component: "A versatile component that shows progress and percentage of two values given. " +
+                    "It calculates the percentage of value from maxValue and displays the percentage rounded down, " +
+                    "and visualizes the progression through the circle. ",
+            }
+        }
     },
     args: {
         maxValue: 54,
@@ -15,6 +22,37 @@ const meta = {
 } satisfies Meta<typeof NovariCircularProgressBar>
 
 export default meta;
-type Story = StoryObj<typeof NovariCircularProgressBar>;
+type Story = StoryObj<typeof meta>;
 
-export const Default: Story = {};
+export const Default: Story = {
+    parameters: {
+        docs: {
+            codePanel: true,
+            description: {
+                story: "These are the colors the component will default to if colors aren't specified:" +
+                    " Novari Blålilla and Burgunder.",
+            }
+        }
+    },
+};
+
+export const Custom: Story = {
+    parameters: {
+        docs: {
+            codePanel: true,
+            description: {
+                story: "You may of course chose any colors you see fit for your application, like for example the" +
+                    " Novari Beige and Korall" +
+                    " like I've done here. Both the Progression Circle itself and the shadow around it will take on" +
+                    " the colors you desire. firstColor specifies the color and shadow on top, and secondColor" +
+                    " specifies the bottom."
+            }
+        }
+    },
+    args: {
+        maxValue: 300,
+        value: 299,
+        firstColor: "#F8ECDC",
+        secondColor: "#F76650",
+    }
+};
