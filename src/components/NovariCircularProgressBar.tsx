e3303e030e760993969a685177ac2defce11a109
sindre <sindre.giske@novari.no>
2025-08-04 12:54:35 +0200

    Fine tuned color coordination by order of specification

--- src/components/NovariCircularProgressBar.tsx
+++ src/components/NovariCircularProgressBar.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useId} from "react";
 
 export interface CircularProgressProps {
     maxValue: number;
@@ -7,14 +7,20 @@ export interface CircularProgressProps {
     secondColor?: string;
 }
 
-export const NovariCircularProgressBar: React.FC<CircularProgressProps> =  ({ maxValue, value, firstColor = "#6B133D", secondColor = "#7F78E8" }) => {
+export const NovariCircularProgressBar: React.FC<CircularProgressProps> =  ({
+                                                                                maxValue,
+                                                                                value,
+                                                                                firstColor = "#7F78E8",
+                                                                                secondColor = "#6B133D"
+}) => {
+    const uniqueId = useId();
 
     const currentValue: number = Math.floor((value / maxValue) * 100);
     const circumference: number = 2 * Math.PI * 56;
     const valueOffset: number = circumference * (1 - (value / maxValue));
 
-    const outerShadow = () => `2px 2px 6px -1px ${firstColor}`
-    const innerShadow = () => `inset 2px 2px 6px -1px ${secondColor}`
+    const innerShadow = () => `inset 2px 2px 6px -1px ${firstColor}`
+    const outerShadow = () => `2px 2px 6px -1px ${secondColor}`
 
     return (
         <div className={"h-32 w-32 relative"}>
@@ -28,22 +34,22 @@ export const NovariCircularProgressBar: React.FC<CircularProgressProps> =  ({ ma
                          boxShadow: innerShadow(),
                      }}
                 >
-                    <p className={"text-3xl pl-2"}>{currentValue}%</p>
+                    <p className={`text-3xl pl-2`}>{currentValue}%</p>
                 </div>
             </div>
             <svg xmlns={"https://www.w3.org/200/svg"} version={"1.1"} width={"128px"} height={"128px"}
                  className={"absolute top-0 left-0"}>
                 <defs>
-                    <linearGradient id={"CircularProgressBarGradient"}>
+                    <linearGradient id={uniqueId}>
                         <stop offset={"0%"}
-                              stopColor={firstColor}/>
-                        <stop offset={"100%"}
                               stopColor={secondColor}/>
+                        <stop offset={"100%"}
+                              stopColor={firstColor}/>
                     </linearGradient>
                 </defs>
                 <g transform="scale(-1,1) translate(-128,0)">
                     <circle cx="64" cy="64" r="56" strokeLinecap={"round"} fill={"none"}
-                            stroke="url(#CircularProgressBarGradient)"
+                            stroke={`url(#${uniqueId})`}
                             strokeDasharray={2 * Math.PI * 56}
                             strokeDashoffset={valueOffset}
                             strokeWidth={"16px"}
